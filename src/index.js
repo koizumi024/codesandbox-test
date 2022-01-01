7f46b6a729f7706ac159ac19af83a08d64d6844d
koizumi024 <hybs11@yahoo.co.jp>
2022-01-02 00:53:40 +0900

    s5

--- src/index.js
+++ src/index.js
@@ -1,13 +1,90 @@
 import "./styles.css";
 
-console.log("ああああああああああ");
-console.log("いい");
+const onclickAdd = () => {
+  // インプットテキストの読み取り - getElementById
+  const addText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  // DOM生成 - createElement
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = addText;
+
+  const completeBtn = document.createElement("button");
+  completeBtn.innerText = "完了";
+  completeBtn.addEventListener("click", () => {
+    // TODO内容を取得
+    const addTarget = completeBtn.parentNode;
+    const addText = addTarget.firstElementChild;
+
+    //divとbtn生成
+    const completeDiv = document.createElement("div");
+    completeDiv.className = "list-row";
+
+    const backBtn = document.createElement("button");
+    backBtn.innerText = "戻す";
+    backBtn.addEventListener("click", () => {
+      // 完了したTODOから削除
+      deleteComplete(backBtn.parentNode);
+
+      const removeText = backBtn.parentNode.firstElementChild;
+
+      const IncompleteDiv = document.createElement("div");
+      IncompleteDiv.className = "list-row";
+
+      IncompleteDiv.appendChild(removeText);
+      IncompleteDiv.appendChild(completeBtn);
+
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerText = "削除";
+      deleteBtn.addEventListener("click", () => {
+        // TODOリストから削除
+        deleteIncomplete(deleteBtn.parentNode);
+      });
+
+      IncompleteDiv.appendChild(deleteBtn);
+
+      document.getElementById("incomplete-list").appendChild(IncompleteDiv);
+    });
+
+    // 追加して配置
+    completeDiv.appendChild(addText);
+    completeDiv.appendChild(backBtn);
+
+    document.getElementById("complete-list").appendChild(completeDiv);
+
+    // TODOリストから削除
+    deleteIncomplete(completeBtn.parentNode);
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "削除";
+  deleteBtn.addEventListener("click", () => {
+    // TODOリストから削除
+    deleteIncomplete(deleteBtn.parentNode);
+  });
+
+  // 子要素として追加 - appendChild
+  div.appendChild(li);
+  div.appendChild(completeBtn);
+  div.appendChild(deleteBtn);
+
+  // 実際に配置
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+// 共通化した関数
+const deleteIncomplete = (target) => {
+  // 子要素を削除 - removeChild
+  document.getElementById("incomplete-list").removeChild(target);
+};
+const deleteComplete = (target) => {
+  // 子要素を削除 - removeChild
+  document.getElementById("complete-list").removeChild(target);
+};
+
+document
+  .getElementById("add-btn")
+  .addEventListener("click", () => onclickAdd());
